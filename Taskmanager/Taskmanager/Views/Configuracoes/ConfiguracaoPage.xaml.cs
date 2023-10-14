8a1c333b6e0cbe988f1fac73fc5b17d8550ccb78
Vinicius Gasparello da Silva <gasparellovinicius@gmail.com>
2023-10-14 15:02:23 -0300

    Tela de Dashboard

---
+++ Taskmanager/Taskmanager/Views/Configuracoes/ConfiguracaoPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Taskmanager.Views.Configuracoes
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ConfiguracaoPage : ContentPage
+    {
+        public ConfiguracaoPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
